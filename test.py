3c09ce8d1c2bff360bae45ba46ff3abfeae6e839
AttilaZsamboki <99214950+AttilaZsamboki@users.noreply.github.com>
2022-06-23 18:49:34 +0200

    Bunch of updates

--- test.py
+++ test.py
@@ -7,7 +7,14 @@ import sys
 from django.db.utils import IntegrityError
 import psycopg2
 
-string = "abs: perimiter"
+good_cols = ["Csomagszám", "Felvétel dátuma", "Súly", "Utánvét", "Utánvét hivatkozás", "Ügyfél hivatkozás", "Címzett neve", "Futár költség", "Kiszállítási cím", "logisztika"]
+bad_cols = []
+for i in df.columns:
+    if i not in good_cols:
+        bad_cols.append(i)
+print(bad_cols)
+df.pop(bad_cols)
+print(df)
 
 string = string.replace(': ', '_')
 print(string)
